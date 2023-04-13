5bd52e02c897b87e839ad66af4a43d3cd95957ff
akajfes <39507748+akajfes@users.noreply.github.com>
2023-04-13 08:26:13 -0600

    Add files via upload

---
+++ js/api/dictionary.js
@@ -0,0 +1,1 @@
+// fetch from the dictionary api here!
---
+++ js/dom/word-item.js
@@ -0,0 +1,15 @@
+/*
+HTML Structure 
+
+<li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto definition">
+        <div class="fw-bold">WORD HERE</div>
+        <p>PARAGRAPH HERE</p>
+    </div>
+    <button class="m-2 btn btn-primary favourite-word">Add To Favourites</button>
+</li>
+
+
+Note: I'm sure you've read the PDF but there will be no marks given
+for using innerHTML or innerText. 
+*/
