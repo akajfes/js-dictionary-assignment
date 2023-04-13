2d64ff2f67c96e90488657eac532c5840a067ba3
Ana Kajfes <akajfes1@nait.ca>
2023-04-13 09:44:14 -0600

    Coding started

--- js/api/dictionary.js
+++ js/api/dictionary.js
@@ -1,1 +1,4 @@
 // fetch from the dictionary api here!
+export function getWord(word) {
+    return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-// link this file in your html!
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {getWord} from './api/dictionary.js';
+
+function getWordData(callback){
+    getWord(callback)
+    .then((res) => res.json())
+    .then((definition) => {
+    callback(definition);
+});
+}
+
+console.log(getDefinition('hello'));
